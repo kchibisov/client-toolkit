c8d0e75b3f5a55b53c64ae54b90ae680f40ec284
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 21:55:50 +0000

    Deploy Smithay/client-toolkit to github.com/Smithay/client-toolkit.git:gh-pages

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["smithay_client_toolkit"] = {"name":"","dirs":[{"name":"data_device","files":["data_device.rs","data_offer.rs","data_source.rs","mod.rs"]},{"name":"keyboard","files":["ffi.rs","keysyms.rs","mod.rs"]},{"name":"pointer","files":["mod.rs","theme.rs"]},{"name":"shell","files":["mod.rs","wl.rs","xdg.rs","zxdg.rs"]},{"name":"utils","files":["mempool.rs","mod.rs"]},{"name":"window","files":["basic_frame.rs","concept_frame.rs","mod.rs"]}],"files":["env.rs","lib.rs","output.rs","surface.rs"]};
+createSourceSidebar();
